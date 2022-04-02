3bd6de893e1b11ae0f71927b9cbc35ae09cc00ec
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-04-01 17:19:48 -0700

    rejectUnauth fix

--- db/models/db-config-actions.js
+++ db/models/db-config-actions.js
@@ -10,13 +10,28 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable], 
+    config,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }  
+  );
 } else {
   sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
-    config
+    config,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    },
   );
 }
 
